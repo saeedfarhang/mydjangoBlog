fed9df6e1804caa357f28890f796b801ef9e52ed
saeed farhang <saeedfarhang.w@gmail.com>
2020-06-28 13:06:16 +0430

    author

--- articles/models.py
+++ articles/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Article(models.Model):
     title = models.CharField(max_length = 100)
@@ -6,7 +7,7 @@ class Article(models.Model):
     body = models.TextField()
     date = models.DateTimeField(auto_now_add = True)
     image = models.ImageField(default='default.jpg',blank=True)
-    # add in author
+    author = models.ForeignKey(User,default=None,on_delete=models.CASCADE)
     def __str__(self):
         return self.title
 
